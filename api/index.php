2f1d1a7befc4d806eeaf6b086320247160e0f592
Vitrolo <viniciusrabelodominguesdasilva@gmail.com>
2024-01-31 14:50:57 -0300

    first

--- api/index.php
+++ api/index.php
@@ -61,9 +61,6 @@
         <label for="email">Email:</label>
         <input type="email" id="email" name="email" required>
 
-        <label for="mensagem">Mensagem:</label>
-        <textarea id="mensagem" name="mensagem" rows="4" required></textarea>
-
         <button type="submit">Enviar</button>
     </form>
 
